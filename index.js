9687a8c4696d5b1f3f4581ba2a68a6c7412a8c9a
Bruno Sampaio <brunoctsb@gmail.com>
2022-08-09 11:05:06 -0300

    dark mode

--- index.js
+++ index.js
@@ -108,3 +108,50 @@ for (let i = 0; i < cards.length; i++){
         audios[i].loop
     })
 }
+
+
+// switcher dark/light mode
+
+const lightButton = document.querySelector("#light")
+const darkButton = document.querySelector("#dark")
+const rootElement = document.documentElement
+
+const lightTheme = {
+    '--bg': '#FFFFFF',
+    '--text-color': 'hsla(240, 6%, 21%, 1)',
+    '--fill-colors': '#323238',
+    '--primary-color': 'hsla(240, 9%, 89%, 1)',
+    '--secondary-color': 'hsla(194, 97%, 31%, 1)',
+}
+const darkTheme = {
+    '--bg': '#121214',
+    '--text-color': '#FFFFFF',
+    '--fill-colors': '#C4C4CC',
+    '--primary-color': '#29292E',
+    '--secondary-color': '#0A3442',
+}
+
+lightButton.addEventListener("click", () => {
+    changeTheme(darkTheme)
+    lightButton.classList.toggle('hide')
+    darkButton.classList.toggle('hide')
+    console.log('hi')
+})
+
+darkButton.addEventListener("click", () => {
+    changeTheme(lightTheme)
+    lightButton.classList.toggle('hide')
+    darkButton.classList.toggle('hide')
+    console.log('hi')
+})
+
+
+function changeTheme(theme){
+    for (let prop in theme){
+        changeProperty(prop, theme[prop])
+    }
+}
+
+function changeProperty(property, value) {
+    rootElement.style.setProperty(property, value)
+}
